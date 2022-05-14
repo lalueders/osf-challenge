a9c5c20e657006de4ef5de6a03bf855da5e05aa0
Lasse Lüders <lasselueders@gmx.de>
2022-05-14 10:51:56 +0200

    production build using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
